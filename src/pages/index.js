003a21a2260ae411e2cc4a4201680ddaa66e2b1f
Caio Lopes de Melo Lindoso <caio.lindoso@MacBook-Pro-von-Caio.local>
2024-12-31 14:39:37 +0100

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,10 @@ function HomepageHeader() {
         {/* Wrapper around buttons */}
         <div className={styles.buttonsWrapper}>
           <div className={styles.buttons}>
+            <img
+              src="../../static/img/ai.png"
+              alt="Tognigy AI logo"
+            />
             <Link
               className="button button--secondary button--lg"
               to="/docs/intro">
